3a0851f1f5fbc183a5c3da4442ed7ff08aea1963
muebau <muebau@gmail.com>
2022-01-30 17:32:22 +0100

    ESP32: add scanWiFi

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,46 @@
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
 
-void setup() {
-  
-  //general
+void scanWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
 
-  //webserver
-  
+  DynamicJsonDocument doc(1024);
+
+  if (n == 0)
+  {
+    doc["networks"][0] = [];
+  }
+  else
+  {
+    for (int i = 0; i < n; ++i)
+    {
+      doc["networks"][i][0] = WiFi.SSID(i);
+      doc["networks"][i][1] = WiFi.RSSI(i);
+      doc["networks"][i][2] = (WiFi.encryptionType(i) == WIFI_AUTH_OPEN);
+    }
+  }
+  serializeJson(doc, Serial);
 }
 
-void loop() {
-  
+void setup()
+{
+
   //general
 
   //webserver
+}
 
+void loop()
+{
+
+  //general
+
+  //webserver
 }
 
 //general
