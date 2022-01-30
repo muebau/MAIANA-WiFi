9029c48823cd060f4846cf8e76d5d13e9e61d529
Toni Kraeser <antonia.kraeser007@stud.fh-dortmund.de>
2022-01-30 18:04:47 +0100

    get requests zusammengesetzt

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
-
 //webserver
 #include <WiFi.h>
 #include <AsyncTCP.h>
@@ -18,11 +17,21 @@
 
 AsyncWebServer server(80);
 
-const char* ssidWebserver = "ulfnet";
-const char* passwordWebserver = "!=hierbinichmenschhierwillichsein44100$%";
+const char *ssidWebserver = "ulfnet";
+const char *passwordWebserver = "!=hierbinichmenschhierwillichsein44100$%";
 
-const char* PARAM_MESSAGE = "message";
-
+const char *PARAM_TYPE = "type";
+const char *PARAM_SSID = "ssid";
+const char *PARAM_PASSWORD = "password";
+const char *PARAM_PORT = "port";
+const char *PARAM_MMSI = "mmsi";
+const char *PARAM_CALLSIGN = "callsign";
+const char *PARAM_VESSELNAME = "vesselname";
+const char *PARAM_VESSELTYPE = "vesseltype";
+const char *PARAM_LOA = "loa";
+const char *PARAM_BEAM = "beam";
+const char *PARAM_PORTOFFSET = "portoffset";
+const char *PARAM_BOWOFFSET = "bowoffset";
 
 //---------------functions-----------------------------
 
@@ -56,45 +65,88 @@ void scanWiFi()
 
 //webserver
 
-void notFound(AsyncWebServerRequest *request) {
-    request->send(404, "text/plain", "Not found");
+void notFound(AsyncWebServerRequest *request)
+{
+  request->send(404, "text/plain", "Not found");
 }
 
+void setup()
+{
 
-void setup() {
-  
   //general
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
 
   //webserver
   WiFi.begin(ssidWebserver, passwordWebserver);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.printf("WiFi Failed!\n");
-        return;
-    }
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
 
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(200, "text/plain", "Hello, world");
-    });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/plain", "Hello, world"); });
 
-    // Send a GET request to <IP>/get?message=<message>
-    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-        String message;
-        if (request->hasParam(PARAM_MESSAGE)) {
-            message = request->getParam(PARAM_MESSAGE)->value();
-        } else {
-            message = "No message sent";
-        }
-        request->send(200, "text/plain", "Hello, GET: " + message);
-    });
+  // GET request /info
+  server.on("/info", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String message;
+              message = "info";
+              request->send(200, "text/plain", "Hello, GET: " + message);
+            });
 
-    server.onNotFound(notFound);
+  // GET request /wifi
+  server.on("/wifi", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String message;
+              if (request->hasParam(PARAM_TYPE) && request->hasParam(PARAM_SSID) && request->hasParam(PARAM_PASSWORD))
+              {
+                message = request->getParam(PARAM_TYPE)->value() + request->getParam(PARAM_SSID)->value() + request->getParam(PARAM_PASSWORD)->value();
+              }
+              else
+              {
+                message = "wifi in leer";
+              }
+              request->send(200, "text/plain", "Hello, wifi: " + message);
+            });
 
-    server.begin();
+  // GET request /protocol
+  server.on("/protocol", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String message;
+              if (request->hasParam(PARAM_TYPE) && request->hasParam(PARAM_PORT))
+              {
+                message = request->getParam(PARAM_TYPE)->value() + request->getParam(PARAM_PORT)->value();
+              }
+              else
+              {
+                message = "protocol in leer";
+              }
+              request->send(200, "text/plain", "Hello, GET: " + message);
+            });
+
+  // GET request /station
+  server.on("/station", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String message;
+              if (request->hasParam(PARAM_MMSI) && request->hasParam(PARAM_CALLSIGN) && request->hasParam(PARAM_VESSELNAME) && request->hasParam(PARAM_VESSELTYPE) && request->hasParam(PARAM_LOA) && request->hasParam(PARAM_BEAM) && request->hasParam(PARAM_PORTOFFSET) && request->hasParam(PARAM_BOWOFFSET))
+              {
+                message = request->getParam(PARAM_MMSI)->value() + request->getParam(PARAM_CALLSIGN)->value() + request->getParam(PARAM_VESSELNAME)->value() + request->getParam(PARAM_VESSELTYPE)->value() + request->getParam(PARAM_LOA)->value() + request->getParam(PARAM_BEAM)->value() + request->getParam(PARAM_PORTOFFSET)->value() + request->getParam(PARAM_BOWOFFSET)->value();
+              }
+              else
+              {
+                message = "station in leer";
+              }
+              request->send(200, "text/plain", "Hello, GET: " + message);
+            });
+
+  server.onNotFound(notFound);
+
+  server.begin();
 }
 
 void loop()
